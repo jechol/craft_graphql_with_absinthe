62ae5813ecfd2566eb6b15c2f8d795fdfc7ef139
Jechol Lee <mr.jechol@gmail.com>
2021-06-22 23:56:54 +0900

    elixir 1.5

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  nixpkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/19.03.tar.gz";
+    sha256 = "sha256:0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
+  }) { };
+in nixpkgs.mkShell {
+  buildInputs = [
+    nixpkgs.darwin.apple_sdk.frameworks.CoreServices
+    nixpkgs.darwin.apple_sdk.frameworks.Foundation
+    nixpkgs.elixir_1_5
+    nixpkgs.rebar
+    nixpkgs.rebar3
+  ];
+}

4f425c48bcd045cdac2ac1609181f9851b3a4a85
Jechol Lee <mr.jechol@gmail.com>
2021-06-23 08:50:27 +0900

    Add platform deps

--- shell.nix
+++ shell.nix
@@ -3,12 +3,13 @@ let
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/19.03.tar.gz";
     sha256 = "sha256:0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
   }) { };
-in nixpkgs.mkShell {
-  buildInputs = [
+  platform = if nixpkgs.stdenv.isDarwin then [
     nixpkgs.darwin.apple_sdk.frameworks.CoreServices
     nixpkgs.darwin.apple_sdk.frameworks.Foundation
-
-    nixpkgs.erlangR20
-    nixpkgs.elixir_1_5
-  ];
+  ] else if nixpkgs.stdenv.isLinux then
+    [ nixpkgs.inotify-tools ]
+  else
+    [ ];
+in nixpkgs.mkShell {
+  buildInputs = [ nixpkgs.erlang nixpkgs.elixir ] ++ platform;
 }

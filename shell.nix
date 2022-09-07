6dfe00aade64ad5c0eddfe7f6883f76e31965f08
Jechol Lee <mr.jechol@gmail.com>
2022-09-07 10:17:28 +0900

    shell.nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,24 @@
 let
   nixpkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/19.03.tar.gz";
-    sha256 = "sha256:0q2m2qhyga9yq29yz90ywgjbn9hdahs7i8wwlq7b55rdbyiwa5dy";
+    url = "https://github.com/jechol/nixpkgs/archive/22.05-otp25-no-jit.tar.gz";
+    sha256 = "sha256:1k4wgcrffdlr7pr401md6dmvfwawcslvcwkv7qjgqqyrx52v130i";
   }) { };
   platform = if nixpkgs.stdenv.isDarwin then [
     nixpkgs.darwin.apple_sdk.frameworks.CoreServices
-    nixpkgs.darwin.apple_sdk.frameworks.CoreFoundation
     nixpkgs.darwin.apple_sdk.frameworks.Foundation
   ] else if nixpkgs.stdenv.isLinux then
     [ nixpkgs.inotify-tools ]
   else
     [ ];
 in nixpkgs.mkShell {
-  buildInputs = [ nixpkgs.erlangR20 nixpkgs.elixir_1_5 nixpkgs.nodejs ]
-    ++ platform;
+  buildInputs = with nixpkgs;
+    [
+      # OTP
+      erlang
+      elixir
+      gcc
+
+      # Node.js
+      nodejs
+    ] ++ platform;
 }

67d75bad3149bc493754e2baefb10de9846fe1b9
Jechol Lee <mr.jechol@gmail.com>
2022-09-21 14:41:39 +0900

    formatter ecto, ecto_sql

--- .formatter.exs
+++ .formatter.exs
@@ -1,9 +1,5 @@
 [
-  import_deps: [:absinthe, :phoenix],
-  inputs: ["**/*.{ex,exs}"],
-  locals_without_parens: [
-    field: 1,
-    has_many: 2,
-    many_to_many: 3
-  ]
+  import_deps: [:absinthe, :phoenix, :ecto, :ecto_sql],
+  inputs: ["*.exs", "{config,lib,test, priv}/**/*.{ex,exs}"],
+  locals_without_parens: []
 ]
